dea28d91da72467663f491037c7e481496dac810
M Елизавета <elisaveta890@gmail.com>
2024-09-23 14:22:05 +0300

    123(3)

--- 123.py
+++ 123.py
@@ -1,3 +1,4 @@
 print("Hello, world!")
-a = input(int("введите первое число"))
-b = input(int("введите второе число"))
+a = int(input("введите первое число"))
+b = int(input("введите второе число"))
+print(a+b)
